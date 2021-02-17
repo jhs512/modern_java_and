866ba8cc2bb3efc85cd1c434feb51c4d72506372
jhs512 <jangka512@gmail.com>
2021-02-17 12:20:56 +0900

    글라이드

--- app/src/main/java/com/example/sbs/myapplication/ui/MainViewModel.java
+++ app/src/main/java/com/example/sbs/myapplication/ui/MainViewModel.java
@@ -6,17 +6,24 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 
+import com.example.sbs.myapplication.util.Util;
+
 import javax.inject.Inject;
 
 import dagger.hilt.android.scopes.ViewModelScoped;
 
 @ViewModelScoped
 public class MainViewModel extends AndroidViewModel {
-    public MutableLiveData<String> lvTitle = new MutableLiveData<>("안녕");
-    public MutableLiveData<String> lvName = new MutableLiveData<>("이름입니다.");
+    public MutableLiveData<String> lvImageUrl = new MutableLiveData<>("https://i.picsum.photos/id/181/536/354.jpg?hmac=CL7yXt5fTwzXUCq3aQahgLLqogtp2sutY853WgJgM_M");
 
     @Inject
     public MainViewModel(@NonNull Application application) {
         super(application);
+
+        Util.preloadImg("https://i.pravatar.cc/350?img=4");
+
+        Util.setTimeout(() -> {
+            lvImageUrl.setValue("https://i.pravatar.cc/350?img=4");
+        }, 5000);
     }
 }
